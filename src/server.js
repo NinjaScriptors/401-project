49ea7cb5e69b41a82773b40f39025e0ea3afc210
mohammadalmoqdad <almekad.mohammad@gmail.com>
2021-01-26 09:11:54 +0200

    try to do the chat

--- src/server.js
+++ src/server.js
@@ -7,13 +7,14 @@ const productRouter = require('./auth/routes/productsRoute.js');
 const app = express();
 const notFound = require('./middleware/404.js');
 const errorHandler = require('./middleware/500.js');
+const chatApp = require('./chatApp/chatServer.js')
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
-
+// app.use(chatApp)
 app.get('/error', (req, res) => {
     throw new Error('a test error');
 });
