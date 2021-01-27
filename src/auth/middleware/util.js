777165bd1084a22d7f1b539b9abdf8f1f2ff4dab
HanaaGhazzi <h.ghazzi@outlook.com>
2021-01-27 16:12:19 +0200

    resolve some issue

--- src/auth/middleware/util.js
+++ src/auth/middleware/util.js
@@ -49,11 +49,8 @@ const isAuth = (req, res, next) => {
 };
 
 const isAdmin = (req, res, next) => {
-  let checkSeller = await User.find({_id:req.user._id});
-  let checkIsAdmin = checkSeller[0].isAdmin;
-
-   console.log('check.isAdmin >>>', checkIsAdmin);
-  if (req.user && checkIsAdmin) {
+  if (req.user && req.user.isAdmin) {
+    
     next();
   } else {
     res.status(401).send({ message: 'Invalid Admin Token' });
