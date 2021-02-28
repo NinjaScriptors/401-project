a13de44b69ec5bc32880b21ac5afd20b2fc44bf2
fati-ma <fatima.atiyya@yahoo.com>
2021-02-28 12:07:37 +0200

    new updates

--- src/auth/models/users/user-schema.js
+++ src/auth/models/users/user-schema.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
   // _id: Schema.Types.,
+  fullName: {type: String, required:true},
   name: { type: String, required: true },
   email: {
     type: String, required: true, unique: true, index: true, dropDups: true,
--- src/auth/routes/userRoute.js
+++ src/auth/routes/userRoute.js
@@ -9,12 +9,13 @@ const basicAuth = require('../middleware/basic-auth.js');
 
 
 userRouter.post('/signin', basicAuth, async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
+  const user = await User.findOne({ name: req.body.name });
   if (user) {
   
     if (bcrypt.compareSync(req.body.password, user.password)) {
       res.send({
         _id: user._id,
+        fullName: user.fullName,
         name: user.name,
         email: user.email,
         isAdmin: user.isAdmin,
@@ -31,12 +32,12 @@ userRouter.post('/signin', basicAuth, async (req, res) => {
 userRouter.post('/signup', async (req, res) => {
 
   const user = new User({
-   
+   fullName: req.body.fullName,
     name: req.body.name,
     email: req.body.email,
     password: bcrypt.hashSync(req.body.password, 8),
-    isAdmin: req.body.isAdmin,
-    isSeller: req.body.isSeller
+    // isAdmin: req.body.isAdmin,
+    // isSeller: req.body.isSeller
   
   });
   if (user.email == 'admin@herfa.com'){
